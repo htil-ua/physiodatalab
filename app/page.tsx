fc89612747c9ff4fcbbb952cf8c1ef61308a009e
Chris Crawford <chris35469@gmail.com>
2026-03-03 14:58:54 -0600

    create loading page

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <h1 className="text-5xl font-bold">Physio Data Lab</h1>
+    </div>
+  );
+}
